a06a7f67244ce91e0d3e14fb4d74ef933bfff56d
kodi venkata keerthan <keerthan1675@gmail.com>
2025-02-18 12:48:46 +0530

    completed milestone 16,17 and 18

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -1,4 +1,5 @@
-const ErrorHandler = require("../utiles/ErrorHandler");
+const ErrorHandler = require("../utils/ErrorHandler");
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.message = err.message || "Internal server Error";
--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -1,6 +1,9 @@
-import LoginPage from './pages/Login'
-import SignupPage from './pages/Signup'
-import Home from "./pages/Home";
-import CreateProduct from './pages/createproduct.jsx';
-import MyProducts from "./pages/myproducts.jsx";
-export {LoginPage,SignupPage,Home,CreateProduct,MyProducts};
+
+import LoginPage from "./pages/Login.jsx";
+import SignupPage from "./pages/Signup.jsx";
+import Home from "./pages/Home.jsx";
+import CreateProduct from "./pages/createProduct.jsx";
+import MyProducts from "./pages/myProducts.jsx";
+import Cart from "./pages/cart.jsx";
+import ProductDetails from "./pages/productDetails.jsx";
+export { SignupPage, LoginPage, Home, CreateProduct, MyProducts, Cart, ProductDetails };
